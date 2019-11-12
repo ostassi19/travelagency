b48187ff06d273f5b8226734828c5bbb33cd6ab4
Ostassi <53826838+Ostassi@users.noreply.github.com>
2019-11-12 23:53:32 +0100

    * trouver un user à partir d'un id * supprimer un user à partir d'un id * mise à jour d'un user

--- src/main/java/com/ditracademy/travelagency1/core/Controller.java
+++ src/main/java/com/ditracademy/travelagency1/core/Controller.java
@@ -1,12 +1,10 @@
 package com.ditracademy.travelagency1.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -18,11 +16,33 @@ public class Controller {
        User user1= new User();
         userRepository.save(user);
         user1.setName("eddddddd");
-        //blala
     }
 
     @GetMapping("/users")
     public List<User> getUsers(){
         return userRepository.findAll();
     }
+
+    @GetMapping("/userid/{id}")
+    public Optional<User> getUserById(@PathVariable int id)
+    {
+        return userRepository.findById(id);
+
+    }
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable int id)
+    {
+        userRepository.deleteById(id);
+    }
+    @PutMapping("user/{id}")
+    public void updateUser(@PathVariable int id, @RequestBody User user)
+    {
+       User user1= userRepository.findById(id).get();
+        user1.setName(user.getName());
+        user1.setAge(user.getAge());
+        userRepository.save(user1);
+    }
+
+
+
 }
